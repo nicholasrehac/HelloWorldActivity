debdd0e3a1bc55c3856e16b85b71ada95ac535c6
Nicholas Rehac <tup88289@temple.edu>
2024-01-30 12:08:17 -0500

    added setOnClickListener() using lambda in comments

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,7 +10,9 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
-    lateinit var displayTextView: TextView
+    private lateinit var displayTextView: TextView
+    private lateinit var nameEditText: EditText
+    private lateinit var clickMeButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +20,27 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
+
+        clickMeButton.setOnClickListener(::onClickMeButtonClicked)
+
+        /*
+        clickMeButton.setOnClickListener {
+            v: View? ->
+            val name = nameEditText.text.toString()
+            val displayTextString = "Hello, $name"
+            displayTextView.text = displayTextString
+         }
+         */
 
         
         // Respond to button click event per specifications
+    }
 
-
+    private fun onClickMeButtonClicked(v: View?) {
+        val name = nameEditText.text.toString()
+        val displayTextString = "Hello, $name"
+        displayTextView.text = displayTextString
     }
 }
